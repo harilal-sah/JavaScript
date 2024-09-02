5b54f4a0d6a0e4751e62b45e713e6f5749728508
Harilal Sah Kanu <160394589+harilal-sah@users.noreply.github.com>
2024-09-02 17:56:24 +0000

    variables and constant done successfully!

---
+++ Unit1/variables.js
@@ -0,0 +1,15 @@
+const accountID = 1294
+let accountEmail = "harilalsahk@gmail.com"
+var accountPassword = 2303051051294
+accountCity = "Vadodara"
+let accountStatus
+
+// accountID = 2582
+
+accountEmail = "hsk@official.com"
+accountCity = "GandhiNagar"
+
+console.log(accountID)
+console.log(accountEmail)
+console.log("We can use table to display all declared variables!\n")
+console.table([accountID, accountEmail, accountPassword, accountCity,accountStatus])
