eda0ceb660e7383ae97cb42b5583c9ebbd39a6b7
Harilal Sah Kanu <160394589+harilal-sah@users.noreply.github.com>
2024-09-03 18:25:58 +0000

    files updated successfully

---
+++ Unit1/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser.
+
+console.log(3
+    + 
+    3); // code readibility should be high neat and clean
+console.log('AlgoRythms');
+
+
+let name = "Hari"
+let age = 18
+let isLoggedIn = false
+let start; 
+
+// number range for integer : => 2 to power 53.
+//bigint for large number such as crypto =, facebook and so on;
+// string : to declare string value : Use/ Prefer Double quoted : " ";
+//boolean => true or false
+//null => standalone value
+//undefined => not assigned any value in it like "start";
+//symbol : while we have to identify or unique we use symbols
+//objects : 
+console.log(typeof null); //type is object
+console.log(typeof undefined); // type of undefined is undefined
