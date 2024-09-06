00964f8c9d4eb92e92a4ecbf9cc9a04388d78345
Harilal Sah Kanu <160394589+harilal-sah@users.noreply.github.com>
2024-09-06 17:01:37 +0000

    comparison between operator

---
+++ Unit1/compareDatatypes.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1) 
+
+console.log(null > 0)
+console.log(null == 0) // null -> converted to 0
+console.log(null >= 0) // value conversion problem
+
+console.log(undefined == 0)
+
