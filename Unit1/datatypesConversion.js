546b8667a3525a5b060b0807dd2cafb8b65cf8cd
Harilal Sah Kanu <160394589+harilal-sah@users.noreply.github.com>
2024-09-06 16:50:33 +0000

    operator conversion

--- Unit1/datatypesConversion.js
+++ Unit1/datatypesConversion.js
@@ -14,7 +14,7 @@ console.log(valueInNumber)
 // when we are converting 33 string to 33 number easily it can be converted 
 
 
-// but when we are trying to convert that "33abc" to string it gives -> NaN(Not a Number)
+// but when we are trying to convert that "33abc" to string it gives -> NaN (Not a Number)
 
 let isLoggedIn = 0
 
@@ -27,3 +27,35 @@ console.log(checkBoolean)
 // "" -> booleans -> false ("" -> empty string)
 // 0 -> booleans -> false
 
+
+
+// ********************* OPerations *****************
+
+let value = 3
+
+let negValue = -value
+
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Coders!"
+
+console.log(str1 + str2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
+
+//if string is first in printing then remaining all behaved as string only as given in no 47.
+
+//assignment operations:
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1,num2,num2)  // all having the value 4
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
